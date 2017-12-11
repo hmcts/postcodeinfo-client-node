0988d8961f261473567da9e3e7c2c0f8f0ab2de3
Tim Jacomb <tim.jacomb@hmcts.net>
2017-12-11 14:24:17 +0000

    Change test syntax

--- __tests__/postcodeInfoClient.ts
+++ __tests__/postcodeInfoClient.ts
@@ -3,20 +3,17 @@ import { PostcodeInfoClient } from '../src'
 const postcodeInfoClient: PostcodeInfoClient = new PostcodeInfoClient('<gibberish token>')
 
 describe('postcodeInfoClient', () => {
-  test('should return valid false if no postcode found', (done) => {
-    postcodeInfoClient.lookupPostcode('123')
-      .then((postcodeResponse => {
+  test('should return valid false if no postcode found', () =>
+    postcodeInfoClient
+      .lookupPostcode('123')
+      .then(postcodeResponse => {
         expect(postcodeResponse.valid).toEqual(false)
-        done()
-      }))
-      .catch(done)
-
-  })
-  test('should return found postcodes', (done) => {
+      })
+  )
+  test('should return found postcodes', () =>
     postcodeInfoClient.lookupPostcode('SN15NB')
-      .then((postcodeResponse => {
+      .then(postcodeResponse => {
         expect(postcodeResponse.addresses[0]).toEqual({})
-      }))
-      .catch(done)
-  })
+      })
+  )
 })
